fbaccbc5eeeb065a8790b1679c6c711eb9358318
Syed Bilal <159046241+bilalshahstudio@users.noreply.github.com>
2025-04-29 16:39:01 +0500

    Basic Pages Components

---
+++ frontend/src/pages/BookTour/BookTour.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BookTour() {
+  return (
+    <div>BookTour</div>
+  )
+}
+
+export default BookTour
---
+++ frontend/src/pages/Tours/AllTours.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AllTours() {
+  return (
+    <div>AllTours</div>
+  )
+}
+
+export default AllTours
